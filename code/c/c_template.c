fad831617bad66c162067be265d4c5a8690e78a7
Tony-Wang <66582560+Electricity-Tony@users.noreply.github.com>
2023-06-25 23:58:31 +0800

    add necessary file
    add the .vscode an c/cpp file
    these file can use to run c or cpp code

---
+++ code/c/c_template.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("the template is successful\n");
+    system("pause");
+    
+    return 0;
+}
---
+++ code/cpp/cpp_template.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "cpp_template is successful" << endl;
+
+    system("pause");
+
+    return 0;
+}
